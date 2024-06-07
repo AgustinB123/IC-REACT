409ef32f5eaf0adc0ae6a96ca43e1a6e15bacf06
AgustinB123 <agustinbrites@gmail.com>
2024-06-07 05:31:50 -0300

    a ver ahora broooo

--- my-app/src/jest.config.js
+++ my-app/src/jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx}",
+        "!src/**/*.test.{js,jsx}",
+        "!src/serviceWorker.js",
+        "!src/setupTests.js"
+      ]
   };
