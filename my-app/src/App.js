09f3d7050d772cc80b72140e7231e2414690fd05
AgustinB123 <agustinbrites@gmail.com>
2024-06-07 02:56:40 -0300

    romper el sonar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,6 +6,34 @@ function App() {
 
   const handleClick = () => {
     setMessage('¡Has hecho clic en el botón!');
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a mi aplicación React</h1>
+        <button onClick={handleClick}>Haz clic aquí</button>
+        {message && <p>{message}</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
+
+
+
+//introducir vulnerabilidades 
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  // Vulnerabilidad: Uso de `eval`
+  const handleClick = () => {
+    setMessage('¡Has hecho clic en el botón!');
+    eval('alert("Esto es inseguro")'); // Uso inseguro de eval
   };
 
   return (
