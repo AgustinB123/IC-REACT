4637ca8420ea58a3e2e1fe97a8f603b71a2562c7
AgustinB123 <agustinbrites@gmail.com>
2024-06-08 16:45:03 -0300

    ahora si wy?

--- my-app/jest.config.js
+++ my-app/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageReporters: ['lcov', 'text', 'clover'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -46,6 +46,7 @@ test('message is not displayed initially', () => {
 });
 
 
+
 ///Pruebas unitarias:
 //La primera prueba (renders welcome message) verifica que el texto "Bienvenido a mi aplicación React" 
 //se renderice correctamente en el componente App.
