eea32c9947f34301c2958534968b66482a9a1138
AgustinB123 <agustinbrites@gmail.com>
2024-06-08 19:24:24 -0300

    subir cambios

--- my-app/jest.config.js
+++ my-app/jest.config.js
@@ -2,9 +2,18 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageReporters: ['lcov', 'text', 'clover'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
   ],
 };
