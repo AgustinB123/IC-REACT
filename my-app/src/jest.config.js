9fe8f4bf583fe5337d545953f5443a5d8a746d25
AgustinB123 <agustinbrites@gmail.com>
2024-06-08 01:02:36 -0300

    cmabios en pack.js yml y jest confi

--- my-app/src/jest.config.js
+++ my-app/src/jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: [
-      "src/App.js"
-    ],
-    coverageDirectory: "coverage",
-    coverageReporters: ["json", "lcov", "text", "clover"]
+    collectCoverageFrom: ['src/**/*.js'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text-summary']
   };
   
