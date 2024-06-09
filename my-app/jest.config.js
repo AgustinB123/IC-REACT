48d2f75c83d5ce85596a90d4c2f129da5b2a86fc
AgustinB123 <agustinbrites@gmail.com>
2024-06-08 21:35:02 -0300

    a ver si sigue

--- my-app/jest.config.js
+++ my-app/jest.config.js
@@ -2,17 +2,12 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
-  },
   moduleNameMapper: {
-    "^react-native$": "react-native-web",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+    "\\.(css|less)$": "identity-obj-proxy"
   },
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
   ],
 };
+
